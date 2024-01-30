787610680c21fd29d017ac27408a07c19ed048a4
Sateesh <sateesh.jh@gmail.com>
2024-01-30 21:56:18 +0400

    Extended GrocerySearchManager with methods to insert groceries (mocked) to the DB and search through the DB items

--- app/src/main/java/com/sateeshjh/groceryappsearch/GrocerySearchManager.kt
+++ app/src/main/java/com/sateeshjh/groceryappsearch/GrocerySearchManager.kt
@@ -2,6 +2,8 @@ package com.sateeshjh.groceryappsearch
 
 import android.content.Context
 import androidx.appsearch.app.AppSearchSession
+import androidx.appsearch.app.PutDocumentsRequest
+import androidx.appsearch.app.SearchSpec
 import androidx.appsearch.app.SetSchemaRequest
 import androidx.appsearch.localstorage.LocalStorage
 import kotlinx.coroutines.Dispatchers
@@ -29,5 +31,42 @@ class GrocerySearchManager(
 
             session?.setSchemaAsync(setSchemaRequest)
         }
+    }
+
+    suspend fun insertGroceries(groceries: List<Grocery>): Boolean {
+        return withContext(Dispatchers.IO) {
+            session?.putAsync(
+                PutDocumentsRequest.Builder()
+                    .addDocuments(groceries)
+                    .build()
+            )?.get()?.isSuccess == true
+        }
+    }
+
+    suspend fun searchGroceries(query: String): List<Grocery> {
+        return withContext(Dispatchers.IO) {
+            val searchSpec = SearchSpec.Builder()
+                .setSnippetCount(10) // max search entries
+                .setRankingStrategy(SearchSpec.RANKING_STRATEGY_USAGE_COUNT)
+                .build()
+
+            val result = session?.search(
+                query,
+                searchSpec
+            ) ?: return@withContext emptyList()
+
+            val page = result.nextPageAsync.get()
+
+            page.mapNotNull {
+                if (it.genericDocument.schemaType == Grocery::class.java.simpleName) {
+                    it.getDocument(Grocery::class.java)
+                } else null
+            }
+        }
+    }
+
+    fun closeSession() {
+        session?.close()
+        session = null
     }
 }
